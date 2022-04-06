10d3d34ca3d8e90d894bbb5ca84e913794850d47
Lucia Sheridan <nluciasheridan@gmail.com>
2022-04-06 18:50:30 -0300

    useEffect + ResultList

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,40 +1,31 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 import SearchBar from './components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from './components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const searchApi = async (searchTerm) => {
-        if (errorMessage) {
-            setErrorMessage('');
-        }
-        try {
-        const response = await yelp.get('/search', {
-           params: {
-               limit: 50,
-               term,
-               location: 'san jose'
-           }
-       });
-       setResults(response.data.businesses);
-        } catch (error) {
-            setErrorMessage('Something went wrong');
-        }
-    };
+    const [searchApi, results, errorMessage] = useResults();
     
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
+
     return (
         <View>
             <SearchBar 
             term={term} 
             onTermChange={setTerm}
-            onTermSubmit={searchApi}
+            onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text> {errorMessage} </Text> : null}
             <Text> We have found {results.length} results </Text>
+            <ResultsList results={filterResultsByPrice('$')}title="Cost Effective" />
+            <ResultsList results={filterResultsByPrice('$$')}title="Bit Pricier"/>
+            <ResultsList results={filterResultsByPrice('$$$')}title="Big Spender"/>
         </View>
     );
 };
