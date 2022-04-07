3d079b4bf43ffe1e7ec99c6f4ac791d37af798e5
Lucia Sheridan <nluciasheridan@gmail.com>
2022-04-07 11:52:42 -0300

    Styling

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import { Text, StyleSheet, FlatList, Image } from 'react-native';
 import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
@@ -18,21 +18,27 @@ const ResultsShowScreen = ({ navigation }) => {
     return null;
   }
 
-  return <View>
-    <Text>{result.name}</Text>
+  return <>
+    <Text style={styles.text}>{result.name}</Text>
     <FlatList
       data={result.photos}
       keyExtractor= {(photo) => photo}
       renderItem={({ item }) => {
         return <Image style={styles.image} source={{ uri: item }}/>
       }}/>
-  </View>
+  </>
 };
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
   image: {
     height: 200,
-    width: 300
+    width: 300,
+    borderRadius: 4,
+    marginBottom: 5
   }
 });
 
--- src/screens/components/ResultsDetail.js
+++ src/screens/components/ResultsDetail.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     width: 250,
     height: 120,
     borderRadius: 4,
-    marginBottom: 5
+    marginBottom: 10
   },
   name: {
     fontSize: 20, 
